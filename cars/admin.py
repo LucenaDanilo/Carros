54fe37544f8389ad420badb22206a99cb32bccce
Lucena.danilo <danilo.lucena.ext@synchro.com.br>
2023-10-13 20:21:36 -0300

    add the brand as foreign key

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from cars.models import Car, Brand
 
 # Register your models here.
 # crudzinho vai rolar aqui ein
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'year', 'value')
+    search_fields = ('model', 'brand',)
+
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    factory_year = models.IntegerField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.model
+    
