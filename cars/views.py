ff00c905370a9527d59858cd6566899f5134199a
Lucena.danilo <danilo.lucena.ext@synchro.com.br>
2024-01-21 00:38:47 -0300

    finishin generic views changes

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 from typing import Any
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from cars.models import Car
 from cars.forms import CarModelForm
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.    
 class CarsListView(ListView):
@@ -20,9 +23,28 @@ class CarsListView(ListView):
 
         return cars
 
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    
+@method_decorator(login_required(login_url='login'), name = 'dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars'
-    
+
+@method_decorator(login_required(login_url='login'), name = 'dispatch')
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('car_detail', kwargs = {'pk': self.object.pk})
+
+@method_decorator(login_required(login_url='login'), name = 'dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars'
